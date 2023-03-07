4dff837e2b5a951cb900338c431b04c6a25973d1
MichaelAGoodrich <mike@cs.byu.edu>
2023-03-07 10:55:45 -0700

    Initial commit

---
+++ Run_GDS_Model.py
@@ -0,0 +1,8 @@
+""" Create a small labeled graph from the Stanford Network Repository.
+    Play with the features of SNAP to learn about graph data science.
+
+    Michael A. Goodrich
+    Brigham Young University
+
+    March 2023    
+"""
